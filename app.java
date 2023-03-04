0cad663fb3e95aae0517cb077dd58536fe177e67
Cantona <trop83@yahoo.com>
2023-03-04 03:43:24 +0000

    Modified

--- app.java
+++ app.java
@@ -2,5 +2,11 @@ NodeJS applications
 This is a great app
 Developing java apps
 .net application
+HEAD
 Python application
 Bug removed
+=======
+Great
+this app is excellent
+This app is excellent for all users
+>>>>>>> a0284e1 (Excellent app for all)
